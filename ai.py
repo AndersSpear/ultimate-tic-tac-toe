aa381c59358c1c771e437696ffd1aff8b4c85331
Lucas LaBuff <lucasbeef00@gmail.com>
2023-02-19 18:14:41 -0500

    random ai exists

--- ai.py
+++ ai.py
@@ -11,3 +11,20 @@ class AI(ABC):
     @abstractmethod
     def clone(self) -> "AI":
         ...
+
+
+# AI which moves randomly
+import random
+class RandomAI(AI):
+    
+    def play(self, board: Main_board) -> int:
+        valid_moves = board.get_valid_moves()
+        if valid_moves:
+            return random.choice(valid_moves)
+        return -1
+    
+    def clone(self):
+        return RandomAI()
+    
+    def __str__(self):
+        return "Random_AI"
--- board.py
+++ board.py
@@ -3,6 +3,8 @@ class Subboard:
         self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.winner = 0
     
+    def get_valid_moves(self):
+        return [i for i,j in enumerate(self.board) if j == 0]
 
     
 
@@ -11,3 +13,9 @@ class Board:
         self.board = [Subboard(), Subboard(), Subboard(), Subboard(), Subboard(), Subboard(), Subboard(), Subboard(), Subboard()]
         self.next_square = -1
         self.next_player = 1
+    
+    def get_valid_moves(self):
+        if self.next_square in (0,1,2,3,4,5,6,7,8):
+            return [self.next_square * 9 + i for i in self.board[self.next_square].get_valid_moves()]
+        else:
+            return [i*9 + j for i,b in enumerate(self.board) for j in b.get_valid_moves()]
