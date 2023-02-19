29ae0f83b63035ed003eb6db00bd2019c7fdc85c
Lucas LaBuff <lucasbeef00@gmail.com>
2023-02-19 18:16:54 -0500

    type hints and stuff

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 from board import Board
+from ai import AI, RandomAI
+
 
 #takes in a list of ai's, pairs them up and returns a list of the winners
-def run_generation(ai_list):
+def run_generation(ai_list: list[AI]) -> list[AI]:
     results = []
     i = 0
     while i < len(ai_list)-1:
@@ -9,9 +11,9 @@ def run_generation(ai_list):
     return results
 
 #takes two ai's, plays a game between them, and returns the winner
-def play_game(ai1, ai2):
+def play_game(ai1: AI, ai2: AI) -> AI:
     board = Board()
 
 
-def generate_ai():
-    print ("idk")
+def generate_ai(count: int) -> list[AI]:
+    return [RandomAI() for _ in range(count)]
