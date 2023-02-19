eb80823ef79d1a8c733a2cdb958b3676923afce9
Lucas LaBuff <lucasbeef00@gmail.com>
2023-02-19 18:47:43 -0500

    anders thinks i did this wrong

--- train.py
+++ train.py
@@ -5,22 +5,21 @@ import random
 
 #takes in a list of ai's, pairs them up and returns a list of the winners
 def run_generation(ai_list: list[AI]) -> list[AI]:
-    results = []
     for i in range(0, len(ai_list), 2):
         winner = play_game(ai_list[i], ai_list[i + 1])
-        results.append(winner)
-        results.append(winner.clone())
-    random.shuffle(results)
-    return results
+        if winner == 1:
+            ai_list[i + 1] = ai_list[i].clone()
+        else:
+            ai_list[i] = ai_list[i + 1].clone()
+    random.shuffle(ai_list)
+    return ai_list
 
 #takes two ai's, plays a game between them, and returns the winner
 def play_game(ai1: AI, ai2: AI) -> AI:
     board = Board()
-    while board.winner == 0:
-        player = board.next_player
-        if not board.make_move(ai1.play(board) if player == 1 else ai2.play(board)):
-            return ai1 if player == 2 else ai2
-    return ai1 if board.winner == 1 else ai2
+    while True:
+        if not board.make_move((ai1, ai2)[board.next_player - 1].play(board)):
+            return (ai1, ai2)[2 - board.next_player]
 
 #generates list of N ai's
 def generate_ai(count: int) -> list[AI]:
